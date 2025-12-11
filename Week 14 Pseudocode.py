2fee8c402352ae26ba89457dce484af2fedc614d
GibbatronGiblet <lbgibson@students.unwsp.edu>
2025-12-11 15:36:57 -0600

    Update Week 14 Pseudocode.py

---
+++ Week 14 Pseudocode.py
@@ -0,0 +1,53 @@
+# Pseudocode: 
+
+# IMPORT Sqlite3 database library  
+
+# CONNECT to the database “students.db” 
+
+# CREATE the cursor object 
+
+# ADD table “Students” 
+
+# INPUT student information 
+
+# SAVE changes 
+
+# CLOSE database connection 
+
+ 
+
+# CONNECT to the database “students.db” 
+
+# CREATE the cursor object 
+
+# DEFINE menu navigation global variables (CREATE, READ, UPDATE, DELETE, EXIT) 
+
+# CREATE navigation menu 
+
+# CREATE user input loop 
+
+# DEFINE create function 
+
+# DEFINE read function 
+
+# DEFINE update function 
+
+# DEFINE delete function 
+
+# DEFINE exit function 
+
+ 
+
+# DISPLAY navigation menu 
+
+# GET user operation choice 
+
+# ASK user to enter info 
+
+# REPEAT input loop until user exits 
+
+# SAVE changes 
+
+# CLOSE database connection 
+
+ 
