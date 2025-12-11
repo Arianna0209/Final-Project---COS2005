95b73a36dc29ce837aa958e78012db6c6c44093d
GibbatronGiblet <lbgibson@students.unwsp.edu>
2025-12-11 15:35:38 -0600

    Add files via upload

---
+++ Week 14 data.py
@@ -0,0 +1,32 @@
+# 8 Fields: 
+
+# Student ID (8-digit number) - INTEGER 
+
+# Name (first middle last) - TEXT 
+
+# Graduation year - INTEGER 
+
+# Primary major or undeclared - TEXT 
+
+# Hometown (city state) - TEXT 
+
+# Email ([first initial][middle initial][last name]@students.unwsp.edu) - TEXT 
+
+# Student type (DE, PSEO, undergraduate, graduate, etc.) - TEXT 
+
+# Campus status (online, commuter, resident) - TEXT 
+
+# Data: 
+
+student_list = [["00000002", Makayla Serenity Andersen, 2027, Mathematics Education, Duluth MN, msandersen@students.unwsp.edu, undergraduate, resident],  
+["00000003", Ryan Jonathon Berkely, 2026, Master of Divinity, Denver CO, rjberkely@students.unwsp.edu, graduate, online],  
+["00000004", Samantha Madison Buchanan, 2029, English, Madison WI, smbuchanan@students.unwsp.edu, Early College, online],  
+["00000005", Emmanuel Santiago Martinez, 2028, Computer Science, Minneapolis MN, esmartinez@students.unwsp.edu, undergraduate, resident],  
+["00000006", Gabriel Alexander Hanson, 2029, Undeclared, Phoenix AZ, gahanson@students.unwsp.edu, undergraduate, online], 
+["00000007", Katherine Rose Owens, 2027, Associate of Arts, Rochester, MN, krowens@students.unwsp.edu, PSEO, online], 
+["00000008", Allan Anthony Green, 2026, Master of Business Administration, St. Paul MN, aagreen@students.unwsp.edu, graduate, resident], 
+["00000009", Eliana Marie Thompson, 2028, Health Science, Williamstown KY, emthompson@students.unwsp.edu, undergraduate, commuter], 
+["00000010", Sofia Mae King, 2027, Doctor of Philosophy, Detroit MI, smking@students.unwsp.edu, graduate, online]] 
+
+print(student_list)
+ 
