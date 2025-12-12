e3d8c872bc87a26df598732a5259c67540c9bda6
GibbatronGiblet <lbgibson@students.unwsp.edu>
2025-12-12 09:40:49 -0600

    Update Week 14 data.py

--- Week 14 data.py
+++ Week 14 data.py
@@ -18,15 +18,17 @@
 
 # Data: 
 
-student_list = [["00000002", Makayla Serenity Andersen, 2027, Mathematics Education, Duluth MN, msandersen@students.unwsp.edu, undergraduate, resident],  
-["00000003", Ryan Jonathon Berkely, 2026, Master of Divinity, Denver CO, rjberkely@students.unwsp.edu, graduate, online],  
-["00000004", Samantha Madison Buchanan, 2029, English, Madison WI, smbuchanan@students.unwsp.edu, Early College, online],  
-["00000005", Emmanuel Santiago Martinez, 2028, Computer Science, Minneapolis MN, esmartinez@students.unwsp.edu, undergraduate, resident],  
-["00000006", Gabriel Alexander Hanson, 2029, Undeclared, Phoenix AZ, gahanson@students.unwsp.edu, undergraduate, online], 
-["00000007", Katherine Rose Owens, 2027, Associate of Arts, Rochester, MN, krowens@students.unwsp.edu, PSEO, online], 
-["00000008", Allan Anthony Green, 2026, Master of Business Administration, St. Paul MN, aagreen@students.unwsp.edu, graduate, resident], 
-["00000009", Eliana Marie Thompson, 2028, Health Science, Williamstown KY, emthompson@students.unwsp.edu, undergraduate, commuter], 
-["00000010", Sofia Mae King, 2027, Doctor of Philosophy, Detroit MI, smking@students.unwsp.edu, graduate, online]] 
+student_list = [[1, "James Christian Gehrke", 2029, "Biochemistry", "Fargo ND", "jcgehrke@students.unwsp.edu", "PSEO", "commuter"], 
+[[2, Makayla Serenity Andersen, 2027, Mathematics Education, Duluth MN, msandersen@students.unwsp.edu, undergraduate, resident],  
+[3, Ryan Jonathon Berkely, 2026, Master of Divinity, Denver CO, rjberkely@students.unwsp.edu, graduate, online],  
+[4, Samantha Madison Buchanan, 2029, English, Madison WI, smbuchanan@students.unwsp.edu, Early College, online],  
+[5, Emmanuel Santiago Martinez, 2028, Computer Science, Minneapolis MN, esmartinez@students.unwsp.edu, undergraduate, resident],  
+[6, Gabriel Alexander Hanson, 2029, Undeclared, Phoenix AZ, gahanson@students.unwsp.edu, undergraduate, online], 
+[7, Katherine Rose Owens, 2027, Associate of Arts, Rochester, MN, krowens@students.unwsp.edu, PSEO, online], 
+[8, Allan Anthony Green, 2026, Master of Business Administration, St. Paul MN, aagreen@students.unwsp.edu, graduate, resident], 
+[9, Eliana Marie Thompson, 2028, Health Science, Williamstown KY, emthompson@students.unwsp.edu, undergraduate, commuter], 
+[10, Sofia Mae King, 2027, Doctor of Philosophy, Detroit MI, smking@students.unwsp.edu, graduate, online]] 
 
 print(student_list)
+
  
